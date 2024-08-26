d95c2cf9484e20b4c39db51b3e1228925f273814
Timmy Jose <zoltan.jose@gmail.com>
2024-08-26 16:49:13 +0530

    feat: checking if the device supports NFC.

--- src/screens/NFCDemo.tsx
+++ src/screens/NFCDemo.tsx
@@ -1,14 +1,33 @@
-import { View, StyleSheet, Button } from 'react-native'
+import { View, StyleSheet, Button, Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParamsList } from '../App'
+import { useEffect, useState } from 'react'
+import nfcManager from 'react-native-nfc-manager'
 
 const NFCDemo = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamsList>>()
 
+  const [hasNfc, setHasNfc] = useState<boolean>(false)
+
+  useEffect(() => {
+    const checkIfNfcIsSupported = async () => {
+      const nfcSupported = await nfcManager.isSupported()
+      setHasNfc(nfcSupported)
+
+      if (nfcSupported) {
+        await nfcManager.start()
+      }
+    }
+
+    checkIfNfcIsSupported()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Button title='Home' onPress={() => navigation.navigate('Home')} />
+      { hasNfc && (<Text>NFC is supported on this device</Text>)}
+      { !hasNfc && (<Text> NFC is not supported on this device</Text>)}
     </View>
   )
 }
